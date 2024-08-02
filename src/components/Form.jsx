830b176a4b46fdb88e14c83cfd363f7acaf96d86
KiwiScreams <screamskiwi3@gmail.com>
2024-08-02 17:38:02 +0400

    button

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,42 @@
 const Form = () => {
   return (
     <>
-    <form>
-        <div className="input-box">
-            <input type="text" name="username" id="username" placeholder="First name"/>
-        </div>
-        <div className="input-box">
-            <input type="text" name="lastname" id="lastname" placeholder="Last name"/>
-        </div>
-        <div className="input-box">
-            <input type="email" name="email" id="email" placeholder="Email address"/>
-        </div>
-        <div className="input-box">
-            <input type="password" name="password" id="password" />
-        </div>
-        <button>CLAIM YOUR FREE TRIAL</button>
-        <p><span>By clicking the button, you are agreeing to our</span> <span>Terms and Services</span></p>
-    </form>
+      <div className="form-section">
+        <form>
+          <div className="input-box">
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="First name"
+            />
+          </div>
+          <div className="input-box">
+            <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              placeholder="Last name"
+            />
+          </div>
+          <div className="input-box">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email address"
+            />
+          </div>
+          <div className="input-box">
+            <input type="password" name="password" id="password" placeholder="password"/>
+          </div>
+          <button>CLAIM YOUR FREE TRIAL</button>
+          <p>
+            <span>By clicking the button, you are agreeing to our</span>{" "}
+            <span>Terms and Services</span>
+          </p>
+        </form>
+      </div>
     </>
   );
 };
